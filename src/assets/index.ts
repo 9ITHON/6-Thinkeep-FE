6fe6cffaf7ec6b69fcd1f6d2fe0f622b1fedb81f
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-04 02:20:57 +0900

    feat: 네비게이션바 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+// AppFooter icon
 export { default as assignment } from "./icons/assignment.svg";
 export { default as assignment_light } from "./icons/assignment_light.svg";
 export { default as favorite } from "./icons/favorite.svg";
@@ -6,3 +7,4 @@ export { default as mail } from "./icons/mail.svg";
 export { default as mail_light } from "./icons/mail_light.svg";
 export { default as settings } from "./icons/settings.svg";
 export { default as settings_light } from "./icons/settings_light.svg";
+ 
