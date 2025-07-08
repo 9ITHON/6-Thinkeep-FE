1e00ac45ccace5f48145786330314dc3d76823eb
kjyook <106497893+kjyook@users.noreply.github.com>
2025-07-08 21:15:53 +0900

    refactor: cardsection components

--- src/components/UI/CardSection.tsx
+++ src/components/UI/CardSection.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 export interface CardSectionProps {
-    title: string;
+    title?: React.ReactNode;
     illustration: React.ReactNode; /*아마도 image아니면 svg icon 주입*/
+    emotion?: string; /*추가적인 감정 표현을 위한 prop*/
+    emotionColor?: string; /*감정 표현에 따른 색상 지정 prop*/
 };
 
-export const CardSection = ({ title, illustration }: CardSectionProps) => {
+export const CardSection = ({ title, illustration, emotion, emotionColor }: CardSectionProps) => {
     return (
-        <div>
-            <h2>{title}</h2>
-            <div>{illustration}</div>
+        <div className="bg-gray1 flex flex-col justify-evenly w-[19rem] h-[22rem] rounded-2xl">
+            {title}
+            <div className="flex justify-center w-full">{illustration}</div>
+            <p className={`w-full text-xl text-center ${emotionColor}`}>{emotion}</p>
         </div>
     );
 };
