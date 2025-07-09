43119cb493ebbd3c8f7ffc6f14253c4612ff35be
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-10 01:45:32 +0900

    feat: 카카오 로그인 버튼 제작

--- src/components/UI/KakaoLoginButton.tsx
+++ src/components/UI/KakaoLoginButton.tsx
@@ -1,19 +1,26 @@
-import { signIn } from 'next-auth/react';
-import React from 'react';
+import { signIn } from "next-auth/react";
+import React from "react";
+import Image from "next/image";
 
 const KakaoLoginButton = () => {
-
   const handleLogin = async () => {
-    await signIn('kakao', { redirect: true, callbackUrl: 'https://localhost:3000/home' })
-    console.log('카카오 로그인 시도');
+    await signIn("kakao", {
+      redirect: true,
+      callbackUrl: "https://localhost:3000/home",
+    });
+    console.log("카카오 로그인 시도");
   };
 
   return (
     <button
       onClick={handleLogin}
-      className="px-4 py-2 font-bold text-black"
+      className="relative w-[361px] h-[62px] bg-[#FEE500] rounded-[12px] flex items-center justify-center gap-2"
     >
-      카카오 로그인
+      <Image src="/kakao_logo.svg" alt="kakao-icon" width={18} height={18} />
+
+      <span className="text-black text-[20px] leading-[24px] font-semibold">
+        카카오 로그인
+      </span>
     </button>
   );
 };
