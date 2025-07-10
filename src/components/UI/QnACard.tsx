55eb7776885eae88e8f6e19cfbb07d77bbc04a57
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-11 00:41:52 +0900

    feat: 캘린더 상세페이지 화면 구현

---
+++ src/components/UI/QnACard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface QnACardProps {
+  number: number;
+  question: React.ReactNode;
+  answer: string;
+}
+
+const QnACard = ({ number, question, answer }: QnACardProps) => {
+  return (
+    <div className="mb-4 bg-gray1 mx-5 rounded-xl p-4">
+      <p className="text-yellow text-[16px] font-medium mb-3 flex gap-2">
+        <div className="w-8 h-8 border-[3px] border-gray2 bg-background rounded-full flex items-center justify-center shrink-0 mt-[2px] ">
+          <span className="text-[18px] leading-[23px] font-semibold text-white">
+            {number}
+          </span>
+        </div>
+        <span className="text-[16px] leading-snug py-1.5">{question}</span>
+      </p>
+
+      <div className="bg-background text-white text-[18px] text-normal rounded-xl p-4">
+        {answer}
+      </div>
+    </div>
+  );
+};
+
+export default QnACard;
