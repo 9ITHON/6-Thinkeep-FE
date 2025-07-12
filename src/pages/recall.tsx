2b7527a530921cc54971b132c97c906ab29a422a
lnylnylnylny <lnylnylnylny@naver.com>
2025-07-13 00:23:13 +0900

    feat: recall화면 arrow 추가

--- src/pages/recall.tsx
+++ src/pages/recall.tsx
@@ -1,72 +1,120 @@
-import BarGraph from '@/components/UI/BarGraph';
-import Button from '@/components/UI/Button';
-import { HeaderBackward } from '@/components/UI/HeaderBacward';
-import QuestionCard from '@/components/UI/QuestionCard';
-import React, { useState } from 'react';
+import BarGraph from "@/components/UI/BarGraph";
+import Button from "@/components/UI/Button";
+import { HeaderBackward } from "@/components/UI/HeaderBacward";
+import QuestionCard from "@/components/UI/QuestionCard";
+import React, { useState } from "react";
+import Image from "next/image";
 
 const RecallPage = () => {
-    const [recallStatus, setRecallStatus] = useState<number>(0);
-    const handleRecall = (status: number) => {
-        setRecallStatus(status);
-    };
+  const [recallStatus, setRecallStatus] = useState<number>(0);
+  const handleRecall = (status: number) => {
+    setRecallStatus(status);
+  };
 
-    const cardList = [
-        {
-            id: 0,
-            title: <h2 className='w-full text-xl font-semibold leading-6 tracking-tight text-center text-white'>
-                    오늘 기분이 어때요?
-                </h2>,
-            emotion: 'nothing',
-            icon: 'flower.svg',
-            iconSize: 228,
-            micMessage: undefined,
-        },
-        {
-            id: 1,
-            title: <h2 className='w-full text-xl font-semibold leading-6 tracking-tight text-center text-white'>
-                    오늘 <span className='text-primary'>누구</span>와 함께<br />시간을 보냈나요?
-                </h2>,
-            emotion: 'nothing',
-            icon: 'flower.svg',
-            iconSize: 228,
-            micMessage: undefined,
-        },
-    ]
+  const cardList = [
+    {
+      id: 0,
+      title: (
+        <h2 className="w-full text-xl font-semibold leading-6 tracking-tight text-center text-white">
+          오늘 기분이 어때요?
+        </h2>
+      ),
+      emotion: "nothing",
+      icon: "flower.svg",
+      iconSize: 228,
+      micMessage: undefined,
+    },
+    {
+      id: 1,
+      title: (
+        <h2 className="w-full text-xl font-semibold leading-6 tracking-tight text-center text-white">
+          오늘 <span className="text-primary">누구</span>와 함께
+          <br />
+          시간을 보냈나요?
+        </h2>
+      ),
+      emotion: "nothing",
+      icon: "flower.svg",
+      iconSize: 228,
+      micMessage: undefined,
+    },
+  ];
 
-    const currentCard = cardList.find((card) => card.id === recallStatus)   //recordStatus에 해당하는 card를 찾음
+  const currentCard = cardList.find((card) => card.id === recallStatus); //recordStatus에 해당하는 card를 찾음
 
-    return (
-        <div className="w-full h-screen bg-background">
-            <div className='flex flex-row gap-4 p-4'>   {/*header*/}
-                <HeaderBackward visible={true} />
-                <BarGraph total={2} recorded={recallStatus} />
-            </div>
-            <div className='flex flex-col items-center w-full h-full gap-5 pt-4'>
-                <div className='flex flex-row justify-around w-full px-4'>    {/*body*/}
-                    <button className='py-2 text-black rounded bg-primary ' onClick={() => handleRecall(recallStatus - 1)}>-1</button>
-                    {currentCard && (
-                        <QuestionCard
-                            title={currentCard.title}
-                            emotion={currentCard.emotion}
-                            iconSize={currentCard.iconSize}
-                            icon={currentCard.icon}
-                            micMessage={currentCard.micMessage}
-                        />
-                    )}
-                    <button className='py-2 text-black rounded bg-primary ' onClick={() => handleRecall(recallStatus + 1)}>+1</button>
-                </div>
-                <div className='flex flex-col items-center w-full h-auto gap-6 px-6'>
-                    <p className='pb-20 font-semibold text-white text-[1.125rem] leading-[1.4375rem] tracking-tight underline decoration-solid decoration-auto underline-offset-auto'>모르겠어요. 다음에 할게여!</p>
-                    <div className='flex flex-row w-full h-auto gap-2'>
-                        <Button text='철수' onClick={() => {}} className='rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-primary text-background'></Button>
-                        <Button text='영희' onClick={() => {}} className='rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-primary text-background'></Button>
-                        <Button text='돌쇠' onClick={() => {}} className='rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-gray2 text-background'></Button>
-                    </div>
-                    <Button text='제출하기' onClick={() => {}} className='rounded-[1.25rem] w-full py-5 text-[1.125rem] font-semibold text-center leading-[1.435rem] tracking-tight bg-primary text-background'></Button>
-                </div>
-            </div>
+  return (
+    <div className="w-full h-screen bg-background">
+      <div className="flex flex-row gap-4 p-4">
+        {" "}
+        {/*header*/}
+        <HeaderBackward visible={true} />
+        <BarGraph total={2} recorded={recallStatus} />
+      </div>
+      <div className="flex flex-col items-center w-full h-full gap-5 pt-4">
+        <div className="flex flex-row justify-around w-full px-4">
+          {recallStatus === 0 ? (
+            <div className="w-[48px]"></div>
+          ) : (
+            <Image
+              src="arrow_left.svg"
+              alt="이전"
+              width={48}
+              height={48}
+              onClick={() => handleRecall(recallStatus - 1)}
+            />
+          )}
+          {currentCard && (
+            <QuestionCard
+              title={currentCard.title}
+              emotion={currentCard.emotion}
+              iconSize={currentCard.iconSize}
+              icon={currentCard.icon}
+              micMessage={currentCard.micMessage}
+            />
+          )}
+
+          {recallStatus === 2 ? (
+            <div className="w-[48px]"></div>
+          ) : (
+            <Image
+              src="arrow_right.svg"
+              alt="이후"
+              width={48}
+              height={48}
+              onClick={() => handleRecall(recallStatus + 1)}
+            />
+          )}
         </div>
-    );
+        <div className="flex flex-col items-center w-full h-auto gap-6 px-6">
+          <p className="pb-20 font-semibold text-white text-[1.125rem] leading-[1.4375rem] tracking-tight underline decoration-solid decoration-auto underline-offset-auto">
+            모르겠어요. 다음에 할게여!
+          </p>
+          <div className="flex flex-row w-full h-auto gap-2">
+            <Button
+              text="철수"
+              onClick={() => {}}
+              className="rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-primary text-background"
+            ></Button>
+            <Button
+              text="영희"
+              onClick={() => {}}
+              className="rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-primary text-background"
+            ></Button>
+            <Button
+              text="돌쇠"
+              onClick={() => {}}
+              className="rounded-[1.25rem] w-full py-8 text-[1.375rem] font-semibold text-center leading-[1.875rem] tracking-tight bg-gray2 text-background"
+            ></Button>
+          </div>
+          <Button
+            text="제출하기"
+            onClick={() => {}}
+            className="rounded-[1.25rem] w-full py-5 text-[1.125rem] font-semibold text-center leading-[1.435rem] tracking-tight bg-primary text-background"
+          ></Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RecallPage;
